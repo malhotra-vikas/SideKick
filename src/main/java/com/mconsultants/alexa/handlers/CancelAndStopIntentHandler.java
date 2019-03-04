c5bff7be10bed5a0747eddf256a62c20a7f3e9ee
SkunkWorks <malhotra.vikas@gmail.com>
2019-03-04 12:07:52 -0500

    APL

--- src/main/java/com/mconsultants/alexa/handlers/CancelAndStopIntentHandler.java
+++ src/main/java/com/mconsultants/alexa/handlers/CancelAndStopIntentHandler.java
@@ -2,29 +2,25 @@ package main.java.com.mconsultants.alexa.handlers;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
-import com.amazon.ask.model.LaunchRequest;
 import com.amazon.ask.model.Response;
 import main.java.com.mconsultants.alexa.utilities.PropertyReader;
 
 import java.util.Optional;
 
-import static com.amazon.ask.request.Predicates.requestType;
+import static com.amazon.ask.request.Predicates.intentName;
 
-public class LaunchRequestHandler implements RequestHandler {
+public class CancelAndStopIntentHandler implements RequestHandler {
+    @Override
     public boolean canHandle(HandlerInput input) {
-        return input.matches(requestType(LaunchRequest.class));
+        return input.matches(intentName("AMAZON.CancelIntent").or(intentName("AMAZON.StopIntent")));
     }
     private static PropertyReader propertyReader = PropertyReader.getPropertyReader();
 
+    @Override
     public Optional<Response> handle(HandlerInput input) {
-        String speechText = "Welcome to Jack and Stack!! How can i help you today? If " +
-                "you dont know what you want try saying what's up";
-        // If the user either does not reply to the welcome message or says something that is not
-        // understood, they will be prompted again with this text.
-        String repromptText = "If you dont know what you want try saying what's up";
+        String speechText = propertyReader.getGoodBye();
         return input.getResponseBuilder()
                 .withSpeech(speechText)
-                .withReprompt(repromptText)
                 .withSimpleCard(propertyReader.getSkillName(), speechText)
                 .build();
     }
